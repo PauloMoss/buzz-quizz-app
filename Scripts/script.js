f28685c409509774da5556e922cbadefc9c6c61b
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-04-21 13:30:48 -0300

    Ajuste scroll proxima pergunta

--- Scripts/script.js
+++ Scripts/script.js
@@ -143,9 +143,9 @@ function selecionarResposta(respostaSelecionada) {
     } 
     respostaSelecionada.classList.add('selecionado');
     respostaSelecionada.removeAttribute("onclick");
-    destacarImagemSelecionada(respostaSelecionada)
-    setTimeout(scrollarProximaPergunta, 2000);
+    destacarImagemSelecionada(respostaSelecionada);
     renderizarResultado();
+    setTimeout(scrollarProximaPergunta, 2000);
 }
 function destacarImagemSelecionada(respostaSelecionada) {
     const perguntaRespondida = respostaSelecionada.parentNode;
@@ -169,12 +169,21 @@ function verificarRespostaCerta(respostasDaPergunta) {
             respostasDaPergunta[i].classList.add("errada")
         }
     }
+    atualizarPerguntaRespondida()
 }
-function scrollarProximaPergunta() {
-    const perguntaNaorespondida = document.querySelector(".naoRespondida");
+let perguntaNaorespondida = document.querySelector(".naoRespondida");
+function atualizarPerguntaRespondida() {
+    let perguntaNaorespondida = document.querySelector(".naoRespondida");
     perguntaNaorespondida.classList.remove('naoRespondida');
     perguntaNaorespondida.classList.add('respondida');
-    perguntaNaorespondida.scrollIntoView();  
+}
+function scrollarProximaPergunta() {
+    perguntaNaorespondida = document.querySelector(".naoRespondida");
+    if(perguntaNaorespondida!==null) {
+        perguntaNaorespondida.scrollIntoView({block: "center", behavior: "smooth"});
+    } else {
+        alert("Fazer scrolar para resultado")
+    } 
 }
 function renderizarResultado() {
     if(contadorDeJogadas===perguntas.length) {
